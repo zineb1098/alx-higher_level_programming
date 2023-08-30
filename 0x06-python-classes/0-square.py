bb1e8694024e927d8f08ae4db7994906730cbbb5
‘zineb1098’ <manssourizineb8@gmail.com>
2023-08-29 17:48:59 -0700

    Pthython fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
+0-square.py
+
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
@@ -16,8 +16,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
 
